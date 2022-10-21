850f1894c934f7bf027bbd1bdaffea84a09c0e4f
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-10-21 10:26:12 +0500

    Add work

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+// let user = confirm('yoshing 18 - 50 oraligidami');
+// console.log(user);
+
+// const curenUser = prompt('wer are you?');
+// console.log(curenUser);
+
+const color = [];
+color[0] = prompt('birinchi rangni kiriting #1');
+color[1] = prompt('birinchi rangni kiriting #2');
+color[2] = prompt('birinchi rangni kiriting #3');
+console.log(color);
